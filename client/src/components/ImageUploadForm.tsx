9ad3a8b67388744bb3caad4df0d6357e73fdb414
Zaker Safdari <zakersafdari@gmail.com>
2024-11-13 23:13:20 +1300

    added some stylings to upload form.

--- client/src/components/ImageUploadForm.tsx
+++ client/src/components/ImageUploadForm.tsx
@@ -30,41 +30,42 @@ const ImageUploadForm = () => {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="fileInput" className="text-xl px-5">Upload Image:</label>
+        <div className="flex flex-col justify-center items-center gap-2 bg-slate-500 p-5">
+          <label htmlFor="fileInput" className="text-xl px-5 block font-semibold p-2">Upload Image:</label>
           <input
             type="file"
             id="fileInput"
             name="file"
-            placeholder="Upload Image"
             onChange={handleFileChange}
             accept="image/*"
             className="text-xl px-3"
             required
           />
         </div>
-        <button type="submit" disabled={uploadStatus === "uploading"}>
+        <button type="submit" disabled={uploadStatus === "uploading"} className="bg-blue-600 text-xl my-3 py-2 px-3 rounded-lg hover:text-white" >
           {uploadStatus === "uploading" ? "Uploading ..." : "Upload"}
         </button>
 
         {uploadStatus === "failed" && <p>Error: {error}</p>}
-        {uploadStatus === "completed" && <p>Upload successful!</p>}
+        {uploadStatus === "completed" && <p className="text-lg font-semibold" >Upload successful!</p>}
       </form>
+      {previewUrl && (
+        <div className="flex justify-center flex-col items-center p-2 gap-2">
+          <h3 className="text-xl">Selected Image Preview:</h3>
+          <img src={previewUrl} alt="Preview" width="200" />
+        </div>
+      )}
+
       {prediction && (
-        <div>
-          <h2>Prediction Result</h2>
+        <div className="flex flex-col justify-center items-center" >
+          <h2 className="z-30 text-2xl font-semibold">Prediction Result</h2>
           <p>
             Vehicle Type: <strong>{prediction.tag}</strong>
           </p>
           <p>Confidence: {(prediction.confidence * 100).toFixed(2)}%</p>
         </div>
       )}
-      {previewUrl && (
-        <div>
-          <h3>Selected Image Preview:</h3>
-          <img src={previewUrl} alt="Preview" width="200" />
-        </div>
-      )}
+ 
     </div>
   );
 };
