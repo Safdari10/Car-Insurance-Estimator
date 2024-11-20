f53883ebd11dd1a5c345667b499b2abc9e3f10d4
Zaker Safdari <zakersafdari@gmail.com>
2024-11-20 13:56:04 +1300

    removed padding from app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ import { ImageUploadProvider } from "./context/ImageUploadContext";
 const App = () => {
   return (
     <ImageUploadProvider>
-      <div className="text-center py-10">
+      <div className="text-center">
         <Header />
         <h1 className="text-4xl text-center font-semibold m-2">
           Turner Insurance
